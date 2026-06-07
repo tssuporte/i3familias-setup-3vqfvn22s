0af1b2000be40202e884109feb97f8c89c31b90b
Skip <skip@usecurling.com>
2026-06-07 07:07:56 -0300

    Fix: Import React in Meals component

--- src/pages/app/Meals.tsx
+++ src/pages/app/Meals.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import pb from '@/lib/pocketbase/client'
 import { startOfWeek, addDays, format, isSameDay } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
